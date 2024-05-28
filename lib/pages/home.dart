45ec66362ffa80f73f3fef81366d6fb229e7c2aa
Esraa Khalifa <esraakhalifa208@yahoo.com>
2024-05-28 16:32:07 +0300

    UpdateProject

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,9 +19,31 @@ class _MyWidgetState extends State<HomePage> {
   double childernNumber = 0;
   String selectedView = '';
 
-  //Created for page: rooms-availabe to access
   String selectedRoomView() => selectedView;
   double guests() => adultsNumber + childernNumber;
+
+  //simple validation check (Guests, selectedRoomView)
+  void onNextButtonPressed() {
+    if (guests() == 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please State the Guests Number')),
+      );
+    } else if (selectedRoomView() == '') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                'Please Choose Your Preferred Room-View from the List "View"')),
+      );
+    } else {
+      slideRightWidget(
+        newPage: RoomsAvailable(
+          guests: guests,
+          selectedRoomView: selectedRoomView,
+        ),
+        context: context,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,100 +62,124 @@ class _MyWidgetState extends State<HomePage> {
       ),
       body: ListView(
         children: [
-          Container(
-            height: 300,
-            color: Colors.brown.shade800,
-            child: Image.asset(
-              'assets/images/hotel.jpg',
-              fit: BoxFit.fitWidth,
-            ),
-          ),
-          SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: CheckDates(
-              label: 'Check-in Date:   ',
-            ),
-          ),
-          CheckDates(
-            label: 'Check-out Date: ',
-          ),
-          SizedBox(height: 8),
-          Divider(color: Colors.brown.shade800),
-          SizedBox(height: 8),
-          SlideBar(
-            value: adultsNumber,
-            guestLabel: 'Adults',
-            onChanged: (newValue) {
-              setState(() {
-                adultsNumber = newValue;
-              });
-            },
-          ),
-          SlideBar(
-            value: childernNumber,
-            guestLabel: 'Children',
-            onChanged: (newValue) {
-              setState(() {
-                childernNumber = newValue;
-              });
-            },
-          ),
-          SizedBox(height: 8),
-          Divider(color: Colors.brown.shade800),
-          SizedBox(height: 8),
-          Text(
-            'Extras:',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.brown.shade800,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Extra(),
-          SizedBox(height: 8),
-          Divider(color: Colors.brown.shade800),
-          SizedBox(height: 8),
-          Text(
-            'View:',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.brown.shade800,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          FlutGroupedButtons<Map<String, dynamic>>(
-            isRadio: true,
-            titleCheckSpace: 10,
-            activeColor: Colors.brown.shade800,
-            idKey: 'id',
-            valueKey: 'View',
-            data: [
-              {'id': 'city', 'View': 'City View'},
-              {'id': 'sea', 'View': 'Sea View'},
-              {'id': 'garden', 'View': 'Garden View'},
-            ],
-            onChanged: (value) {
-              setState(() {
-                selectedView = value;
-              });
-              print(value);
-            },
-          ),
-          SizedBox(height: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Card(
+                elevation: 8,
+                color: Colors.brown.shade800,
+                child: Container(
+                  height: 240,
+                  child: Image.asset(
+                    'assets/images/hotel.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              )),
           Padding(
-            padding: const EdgeInsets.only(bottom: 40),
-            child: TextButton(
-                onPressed: () {
-                  slideRightWidget(
-                      newPage: RoomsAvailable(
-                          guests: guests, selectedRoomView: selectedRoomView),
-                      context: context);
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.brown.shade800),
-                child: Text('Next', style: TextStyle(fontSize: 16))),
-          ),
+              padding: const EdgeInsets.all(5),
+              child: Card(
+                surfaceTintColor: Colors.white,
+                elevation: 8,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CheckDates(label: 'Check-in Date:  '),
+                      CheckDates(
+                        label: 'Check-out Date:',
+                      ),
+
+                      SizedBox(height: 8),
+                      Divider(color: Colors.brown.shade800),
+                      SizedBox(height: 8),
+
+                      //calling slidebar
+                      SlideBar(
+                        value: adultsNumber,
+                        guestLabel: 'Adults',
+                        onChanged: (newValue) {
+                          setState(() {
+                            adultsNumber = newValue;
+                          });
+                        },
+                      ),
+                      SlideBar(
+                        value: childernNumber,
+                        guestLabel: 'Children',
+                        onChanged: (newValue) {
+                          setState(() {
+                            childernNumber = newValue;
+                          });
+                        },
+                      ),
+
+                      //calling extra
+                      SizedBox(height: 20),
+                      Text(
+                        'Extras:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.brown.shade800,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Extra(),
+                      SizedBox(height: 20),
+                      Text(
+                        'View:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.brown.shade800,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      FlutGroupedButtons<Map<String, dynamic>>(
+                        isRadio: true,
+                        titleCheckSpace: 10,
+                        activeColor: Colors.brown.shade800,
+                        idKey: 'id',
+                        valueKey: 'View',
+                        data: [
+                          {'id': 'city', 'View': 'City View'},
+                          {'id': 'sea', 'View': 'Sea View'},
+                          {'id': 'garden', 'View': 'Garden View'},
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            selectedView = value;
+                          });
+                          print(value);
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            onNextButtonPressed();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            backgroundColor: Colors.brown.shade800,
+                          ),
+                          child: Text(
+                            'Next',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              )),
         ],
       ),
     );
